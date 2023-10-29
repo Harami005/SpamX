d642e9231347eaeb3f6a0b620f9ba32d2e3270ee
Madara <147428837+Madara1188@users.noreply.github.com>
2023-10-29 23:40:24 +0530

    Update leave.py

--- SpamX/modules/for_bot/leave.py
+++ SpamX/modules/for_bot/leave.py
@@ -23,7 +23,7 @@ async def leave(SpamX: Client, e: Message):
     if len(e.text) > 7:
         chat = rizoel[0]
         try:
-           if chat in [--1001963474655, 1918848221, "@billaganghh"]:
+           if chat in [-1002025766361, 6070538973, "@MADARA_WORLD"]:
               return
            await SpamX.leave_chat(chat)
            await e.reply_text("**Left Successfully ✅ **")
